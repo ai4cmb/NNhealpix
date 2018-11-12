b770851a7fbc3339d1e3187a13ed4b221f0daffa
Nicoletta Krachmalnicoff <niki@dhcp-082.ap.sissa.it>
2018-11-12 15:34:32 +0100

    solving tab problem

--- nnhealpix/layers/blocks.py
+++ nnhealpix/layers/blocks.py
@@ -187,12 +187,10 @@ def ResConvNeighbours(
     def f(x):
         shortcut = x
         y = ConvNeighbours(nside, kernel_size, filters,use_bias=use_bias, trainable=trainable)(x)
-	    if BatchNorm:
-            y = keras.layers.BatchNormalization(axis=1, epsilon=1e-5)(y)
+        y = keras.layers.BatchNormalization(axis=1, epsilon=1e-5)(y)
         y = keras.layers.Activation('relu')(y)
         y = ConvNeighbours(nside, kernel_size, filters,use_bias=use_bias, trainable=trainable)(y)
-        if BatchNorm:
-            y = keras.layers.BatchNormalization(axis=1, epsilon=1e-5)(y)
+        y = keras.layers.BatchNormalization(axis=1, epsilon=1e-5)(y)
         y = keras.layers.Activation('relu')(y)
         y = keras.layers.Add()([y, shortcut])
         return y
